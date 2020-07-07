879ee6a59785aac5c77ce78ef02ff3c7e64596b3
halikhani <h.alikhani.k@gmail.com>
2020-07-07 17:19:01 +0430

    first commit-bug fix

--- arduino_board/arduino_board.ino
+++ arduino_board/arduino_board.ino
@@ -1,4 +1,4 @@
-#include <Adafruit_ESP8266.h>
+
 #include <ESP8266HTTPClient.h>
 
 
@@ -124,67 +124,7 @@ void loop() {
     http.end();
      
 
-      
-  }  
-
-
-
-  
-  
-  
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-  // ==================soil humidity sensor=====================
-  HS_val = analogRead(HS_Pin);
-  Serial.print("soil humidity: ");  
-  Serial.println(HS_val);                                   //todo: send to raspberry pi
-  //(sensorval>1000: sensor disconnected or is not in the soil)
-  //(600<sensorval<1000: soil is dry)
-  //(370<sensorval<600: soil is humid)
-  //(sensorval<370: sensor in water)
-  
-
-
-
-  // ==================temperature sensor========================
-
-  int temp = analogRead(tmp);
-  float volts = (temp / 965.0) * 5;
-  float c = (volts - 0.5) * 100;
-
-  Serial.print("temperature(in centigrade: )");
-  Serial.println(c);
-
-
-
-  // ==================light sensor========================
-  
-  PC_val = analogRead(PC_Pin);
-
-  Serial.print("photocell output: )");
-  Serial.println(PC_val);
-
- // PC_val < 10: Dark
- // 10<PC_val<200: Dim
- // 200<PC_val<500: Light
- // 500<PC_val<800: Bright
- // 800<PC_val: Very Bright
-
-
-  delay(60000); 
+    
+  }    
 
 }
